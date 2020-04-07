fad2f809dab8aa620abcd7e6d5587fe1bdf52e77
Sophia Sun <sophiaphia@live.com>
2020-04-06 17:39:24 -0700

    added send functionality

--- portal/etc/split_videos.py
+++ portal/etc/split_videos.py
@@ -1,7 +1,6 @@
-import os
-import argparse
-import sys
-import cv2 # gotta install on pis
+import os, sys
+import cv2 # only local computer need this
+from portal import get_portal_ip_list
 
 
 def sanitize_filename(data):
@@ -30,6 +29,7 @@ def split_video(path=None):
 	print("width", width)
 	print("height", height)
 	print("video ratio:", ratio)
+	
 	# one side of portal is 6x3
 	output_ext = "mp4"
 	size_x = 1280
@@ -56,7 +56,7 @@ def split_video(path=None):
 	border_width = border_x / total_x * width
 	border_height = border_y / total_y * height
 
-	output_path = "./grid_video"
+	output_path = truncated_output
 
 	if sys.version_info[0] < 3:
 		if not os.path.exists(output_path):
@@ -64,14 +64,14 @@ def split_video(path=None):
 	else:
 		os.makedirs(output_path, exist_ok=True)
 
-	# tvs are indexed by reading sequences
+	# tvs are indexed by top-left-is-0 coordinate
 	# w:h:x:y out.mp4
 
 	cmd_template = 'ffmpeg -i {} -vf "crop={}:{}' \
 					.format('"' + path + '"', frame_width, frame_height) \
-					 + ':{}:{}" grid_video/{}'
+					 + ':{}:{}" {}'
 
-
+	fnames = []
 	for x_indx in range(6):
 		for y_indx in range(3):
 			x = x_indx * (frame_width + border_width)
@@ -79,28 +79,39 @@ def split_video(path=None):
 			idx = 3 * x_indx + y_indx
 			# should we have our index correspond with our hostname? no, that's confusing since
 			# the hostnames already make no sense in regards to the physical map
-			filename = "%s-%d.%s" % (truncated_output, idx, output_ext)
+			filename = "./%s/%d.%s" % (truncated_output, idx, output_ext)
+			fnames.append(filename)
 			command = cmd_template.format(x, y, filename)
 			print(command)
 			os.system(command)
+	return fnames, truncated_output
+
+
+def send_videos(fnames, directory):
+
+	cmd_template = "scp {} {}" # scp ./gen/dir/index pi_index_ip/dir/left or right 
+	naming_mode = ["movie1", "movie2"]
+
+	# want a mapping of: monitor index -> ip address + left or right
+	for idx in range(18):
+		portal = "portal%02d" % (1+int(idx/2))
+		leftright = idx % 2
+
+		if leftright == 0:
+			os.system('ssh %s "mkdir -p ~/%s"' % (portal, directory))
+		cmd = "scp %s %s:~/%s/%s" % (fnames[idx], portal, directory, naming_mode[leftright])
+		print(cmd)
+		os.system(cmd)
 
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
 		path = sys.argv[1]
 	else:
-		path = "/Users/vs/personal/sync/projects/brahman/portal/media/portal-movies/Orcas following my boat....underwater view!-tau8up04Igo.mp4"
-	split_video(path)
+		path = "./gen/test/test.MP4"
+		#path = "/Users/vs/personal/sync/projects/brahman/portal/media/portal-movies/Orcas following my boat....underwater view!-tau8up04Igo.mp4"
+	
+	fnames, directory = split_video(path)
+	send_videos(fnames, directory)
 
-	"""
-	parser = argparse.ArgumentParser()
-	parser.add_argument('file', type=str, help='video filename')
-	args = parser.parse_args()
 
-	#TODO: check file format is legal
-	if args.file:
-		split_video(args.file)
-	else:
-		# test vid, worst possible filename woo
-		path = "/Users/vs/personal/sync/projects/brahman/portal/media/portal-movies/Orcas following my boat....underwater view!-tau8up04Igo.mp4"
-	"""
